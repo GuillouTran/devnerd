44e27f4a653262e0d584614fa44752a919fd6e13
hicham ahendouz <ahendouz.dev@gmail.com>
2019-01-15 09:42:51 +0100

    Create a Navbar component which contains Search component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import Search from "./Search";
+
 const Navbar = () => {
-  return <nav>Nav</nav>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        marginBottom: "1rem"
+      }}
+    >
+      <h1 className="app-logo">MyTodo</h1>
+      <Search />
+    </div>
+  );
 };
 
 export default Navbar;
